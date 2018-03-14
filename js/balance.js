c4b3284cf4885e2ac86ee287a7c5ca36d01c56e6
nickboon <boonnick@hotmail.com>
2018-03-14 00:35:17 +0000

    Added unit tests for daily balance

--- js/balance.js
+++ js/balance.js
@@ -3,7 +3,20 @@
     if (typeof module != 'undefined' && module.exports)
         module.exports = factory();
 })(function() {
-    var balance = {};
+    function balance(balanceObject) {
+        if (balanceObject == undefined || balanceObject == null)
+            throw 'Balance can\'t be null or undefined.';
+
+        var sum = 0;
+
+        if (balanceObject.credit)
+            balanceObject.credit.forEach(entry => sum += entry.amount);
+
+        if (balanceObject.debit)
+            balanceObject.debit.forEach(entry => sum -= entry.amount);
+
+        return sum;
+    }
 
     return balance;
 });
--- tests/balanceTests.js
+++ tests/balanceTests.js
@@ -1,2 +1,69 @@
 var test = require('tape');
 var balance = require('../js/balance.js');
+
+test('balance() with null argument', function(assert) {
+    assert.throws(function() {
+        balance();
+    }, 'should throw an exception');
+    assert.end();
+});
+
+test('balance() given null', function(assert) {
+    assert.throws(function() {
+        balance(null);
+    }, 'should throw an exception');
+    assert.end();
+});
+
+test('balance() given obejct with no credit or debit entries', function(assert) {
+    var expected = 0;
+
+    var actual = balance({});
+
+    assert.equal(actual, expected, 'should return 0');
+    assert.end();
+});
+
+test('balance() given a single credit entry', function(assert) {
+    var expected = 10;
+
+    var actual = balance({
+        "credit": [{
+            "amount": expected
+        }]
+    });
+
+    assert.equal(actual, expected, 'should return the daily value of that entry');
+    assert.end();
+});
+
+test('balance() given a single debit entry', function(assert) {
+    var debit = 10;
+    var expected = -debit;
+
+    var actual = balance({
+        "debit": [{
+            "amount": debit
+        }]
+    });
+
+    assert.equal(actual, expected, 'should return the daily value of that entry');
+    assert.end();
+});
+
+test('balance() given equal debit and credit entries', function(assert) {
+    var amount = 10;
+    var expected = 0;
+
+    var actual = balance({
+        "debit": [{
+            "amount": amount
+        }],
+        "credit": [{
+            "amount": amount
+        }]
+    });
+
+    assert.equal(actual, expected, 'should return 0');
+    assert.end();
+});
