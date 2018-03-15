0ed45bf848f8e8aaba4731d7700d7998e4c68077
nickboon <boonnick@hotmail.com>
2018-03-15 00:16:19 +0000

    Removed es6 => syntax

--- js/balance.js
+++ js/balance.js
@@ -1,18 +1,17 @@
 var amountPerDay = require('./cycles.js');
 
+var total = 0;
+var credit = function(entry) { total += amountPerDay(entry); };
+var debit = function(entry) { total -= amountPerDay(entry); };
+
 var balance = function(entries) {
     if (typeof entries != 'object')
         throw 'entries must be an object.';
 
-    var sum = 0;
-
-    if (entries.credit)
-        entries.credit.forEach(entry => sum += amountPerDay(entry));
-
-    if (entries.debit)
-        entries.debit.forEach(entry => sum -= amountPerDay(entry));
-
-    return sum;
+    total = 0;
+    if (entries.credit) entries.credit.forEach(credit);
+    if (entries.debit) entries.debit.forEach(debit);
+    return total;
 };
 
 module.exports = balance;
