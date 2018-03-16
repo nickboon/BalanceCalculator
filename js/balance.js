d3261a904e1150aa649fffa80bbe94fa6a997137
nickboon <boonnick@hotmail.com>
2018-03-16 00:37:08 +0000

    added eslint

--- js/balance.js
+++ js/balance.js
@@ -1,17 +1,19 @@
 var amountPerDay = require('./cycles.js');
 
-var total = 0;
-var credit = function(entry) { total += amountPerDay(entry); };
-var debit = function(entry) { total -= amountPerDay(entry); };
-
-var balance = function(entries) {
+var sum = function(entries) {
     if (typeof entries != 'object')
         throw 'entries must be an object.';
 
-    total = 0;
-    if (entries.credit) entries.credit.forEach(credit);
-    if (entries.debit) entries.debit.forEach(debit);
-    return total;
+    var balance = 0;
+    if (entries.credit)
+        entries.credit.forEach(function(entry) {
+            balance += amountPerDay(entry);
+        });
+    if (entries.debit)
+        entries.debit.forEach(function(entry) {
+            balance -= amountPerDay(entry);
+        });
+    return balance;
 };
 
-module.exports = balance;
+module.exports = sum;
