cbc9db1f467ca9f90d92112d0e40619a0792445e
nickboon <boonnick@hotmail.com>
2018-03-29 22:56:59 +0100

    Added colour to ratio calculation

--- js/colours.js
+++ js/colours.js
@@ -1,17 +1,39 @@
-const defaultCreditColour = [128, 192, 128];
-const defaultDebitColour = [192, 128, 128];
-const zeroColour = [128, 128, 128];
+const baseColour = 128;
+const maxColour = 256;
+const midColour = (maxColour + baseColour) / 2;
 
-const calculateAmountColour = function(amount) {
-    if (typeof amount != 'number')
-        throw 'Amount must be a number.';
+const buildRed = (red, other) => [red, other, other];
+const buildGreen = (green, other) => [other, green, other];
+const buildGrey = (colour) => [colour, colour, colour];
 
-    if (amount > 0)
-        return defaultCreditColour;
-    else if (amount < 0)
-        return defaultDebitColour;
+const defaultCreditColour = buildGreen(midColour, baseColour);
+const maxCreditColour = buildGreen(maxColour, 0);
+const defaultDebitColour = buildRed(midColour, baseColour);
+const maxDebitColour = buildRed(maxColour, 0);
+const zeroBalanceColour = buildGrey(baseColour);
 
-    return zeroColour;
+const calculateAmountColour = (amount, max, min) => {
+    if (typeof amount != 'number') throw 'Amount must be a number.';
+
+    if (max) return calculateColourToRatio(amount, max, min);
+
+    if (amount > 0) return defaultCreditColour;
+    if (amount < 0) return defaultDebitColour;
+
+    return zeroBalanceColour;
 };
+
+const calculateColourToRatio = (amount, max, min) => {
+    min = min || -max;
+
+    if (amount >= max) return maxCreditColour;
+    if (amount > 0) return buildColourToRatio(buildGreen, max / amount);
+    if (amount <= min) return maxDebitColour;
+    if (amount < 0) return buildColourToRatio(buildRed, min / amount);
+
+    return zeroBalanceColour;
+};
+
+const buildColourToRatio = (builder, ratio) => builder(maxColour / ratio, baseColour / ratio);
 
 module.exports = calculateAmountColour;
--- tests/colours.Tests.js
+++ tests/colours.Tests.js
@@ -17,18 +17,59 @@ test('colour() given 0', assert => {
     assert.end();
 });
 
-test('colour() given a positive amount',
-    assert => {
-        assert.isEquivalent(
-            colour(1), [128, 192, 128],
-            'should return default credit colour');
-        assert.end();
-    });
+test('colour() given a positive amount', assert => {
+    assert.isEquivalent(
+        colour(1), [128, 192, 128],
+        'should return default credit colour');
+    assert.end();
+});
 
-test('colour() given a negative amount',
-    assert => {
-        assert.isEquivalent(
-            colour(-1), [192, 128, 128],
-            'should return default debit colour.');
-        assert.end();
-    });
+test('colour() given a negative amount', assert => {
+    assert.isEquivalent(
+        colour(-1), [192, 128, 128],
+        'should return default debit colour.');
+    assert.end();
+});
+
+
+test('colour() given a max argument and an amount equal to max', assert => {
+    assert.isEquivalent(
+        colour(1, 1), [0, 256, 0],
+        'should return the max credit colour');
+    assert.end();
+});
+
+test('colour() given a max argument only and an amount equal to negative max', assert => {
+    assert.isEquivalent(
+        colour(-1, 1), [256, 0, 0],
+        'should return the max debit colour');
+    assert.end();
+});
+
+test('colour() given a max argument and an amount equal to half max', assert => {
+    assert.isEquivalent(
+        colour(1, 2), [64, 128, 64],
+        'should return a colour halfway between max credit and zero');
+    assert.end();
+});
+
+test('colour() given a max argument only and an amount equal to -half max', assert => {
+    assert.isEquivalent(
+        colour(-1, 2), [128, 64, 64],
+        'should return a colour halfway between max debit and zero');
+    assert.end();
+});
+
+test('colour() given a min argument and an amount less than min', assert => {
+    assert.isEquivalent(
+        colour(-3, 4, -2), [256, 0, 0],
+        'should return the max debit colour');
+    assert.end();
+});
+
+test('colour() given a min argument and an amount equal to half min', assert => {
+    assert.isEquivalent(
+        colour(-2, 1, -4), [128, 64, 64],
+        'should return a colour halfway between max debit and zero');
+    assert.end();
+});
